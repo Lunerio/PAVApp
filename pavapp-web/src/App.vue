3e2a545ce38f3a94254d545f34f156fbde927264
carlosaraque22 <cararqueu@gmail.com>
2021-06-08 21:45:11 +0000

    fixing

--- pavapp-web/src/App.vue
+++ pavapp-web/src/App.vue
@@ -2,13 +2,13 @@
   <div id="app">
     <img alt="PAVAapp Logo" src="./assets/PAVApp-logo.png" />
     <HelloWorld msg="PAVApp" />
-    <input type="button" @click="cambiarTheme()" :value="'Cambiar '+theme" />
+    <input type="button" @click="changeTheme()" :value="'Cambiar '+theme" />
   </div>
 </template>
 
 <script>
-import "./assets/theme-black.css";
-import "./assets/theme-white.css";
+import "./assets/black-theme.css";
+import "./assets/white-theme.css";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
@@ -21,33 +21,22 @@ export default {
   },
   data() {
     return {
-      theme: "black-theme"
+      theme: "white-theme"
     }
   },
   methods: {
-    changeTheme: function () {
-      this.cambiarBodyClass()
-  },
-    changeBodyClass: function () { 
-      if (this.theme === "black") {
-         document.body.setAttribute("class", "black-theme")
+    changeTheme() {
+      this.theme = (this.theme === "black-theme") ? "white-theme" : "black-theme";
+      this.changeBodyClass()
+    },
+
+    changeBodyClass () { 
+      if (this.theme === "white-theme") {
+         document.body.setAttribute("class", "white-theme")
       } else {
-        document.body.setAttribute("class", "white-theme")
+        document.body.setAttribute("class", "black-theme")
       }
     }
   }
 };
 </script>
-
-<style>
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  margin-top: 60px;
-}
-.box {
-  padding: 10px;
-  display: inline-block;
-}
-</style>
