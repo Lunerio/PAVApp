563aeb89f91e6a6f880fca19acb08bd51a843ac5
carlosaraque22 <cararqueu@gmail.com>
2021-06-10 23:21:22 +0000

    add messages to interface

--- pavapp-web/src/components/HelloWorld.vue
+++ pavapp-web/src/components/HelloWorld.vue
@@ -1,21 +1,20 @@
-<template>
+o<template>
   <div class="hello">
     <p>
-      Off
       <label class="switch">
         <input v-on:click="onSwitch" type="checkbox" :checked="encendida" />
         <span class="slider round"></span>
       </label>
-      On
     </p>
     <div>{{ status }}</div>
+    <div>{{ failed }}</div>
     <div class="box">
       <round-slider
         v-bind:change="onChange"
         v-model="sliderValue"
         v-bind:tooltipFormat="degrees"
         handleSize="35"
-        radius="150"
+        radius="140"
         circleShape="pie"
         startAngle="-45"
         endAngle="180"
@@ -41,35 +40,41 @@ export default {
       encendida: false,
       sliderValue: 0,
       status: "",
+      failed: "",
     };
   },
   props: {
     msg: String,
   },
   async mounted() {
-    const actualstatus = await axios.get("http://192.168.1.111/actualStatus");
-    if (actualstatus.data == "1") {
-      this.encendida = true;
-      const actualtemp = await axios.get("http://192.168.1.111/actualTemp");
-      this.sliderValue = actualtemp.data;
-      setInterval(async () => {
-        const actualstatus = await axios.get(
-          "http://192.168.1.111/actualStatus"
-        );
-        if (actualstatus.data == "1") {
-          this.status = "Heating up, please hold";
-        }
-        if (actualstatus.data == "0") {
-          this.encendida = false;
-          this.status = "You can take your kettle now";
-          stop;
-        }
-      }, 500);
-    }
-    if (actualstatus.data == "0") {
-      this.encendida = false;
-    }
-  },
+    try { 
+      const actualstatus = await axios.get("http://192.168.1.111/actualStatus");
+      if (actualstatus.data == "1") {
+        this.encendida = true;
+        const actualtemp = await axios.get("http://192.168.1.111/actualTemp");
+        this.sliderValue = actualtemp.data;
+        setInterval(async () => {
+          const actualstatus = await axios.get(
+            "http://192.168.1.111/actualStatus"
+          );
+          if (actualstatus.data == "1") {
+            this.status = "Heating up, please hold";
+          }
+          if (actualstatus.data == "0") {
+            this.encendida = false;
+            this.status = "You can take your kettle now";
+            stop;
+          }
+        }, 500);
+      }
+      if (actualstatus.data == "0") {
+        this.encendida = false;
+      }
+    } catch (error) {
+      console.log(error);
+      this.failed = "The kettle is disconnected";
+    } 
+},
   // http://192.168.1.111
   methods: {
     degrees: function (args) {
